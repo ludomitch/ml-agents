885a61c5ebb3acdbcc076a156c9180f739024b63
Benjamin <bb1010@ic.ac.uk>
2020-04-22 18:05:03 +0100

    delete animalai

--- examples/train_demonstration.py
+++
@@ -1,3 +0,0 @@
-# TODO: for later release, will need to add a recorder within the compiled
-# TODO: environment, (if that's feasible at all)
-# TODO: https://github.com/Unity-Technologies/ml-agents/blob/master/docs/Training-Imitation-Learning.md
